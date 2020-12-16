75448914461b4095b59a0484265155498e4b1f44
YiPing <pan.yiping.fi@gmail.com>
2020-12-16 21:05:54 +0800

    Practice io

---
+++ io.hs
@@ -0,0 +1,9 @@
+main = do putStrLn ("             ^  -  ^\n\
+                    \            ( .   . )\n\
+                    \              =>;<=\n\
+                    \             /     \\\n\
+                    \            |       |\n\
+                    \ * Rational Arithmetic Evaluation *\n\
+                    \   Example: (1%2) + (3%4) * (5%6)")
+          inputString <- getLine 
+          putStrLn ("Your input is" ++ inputString ++ "!")
