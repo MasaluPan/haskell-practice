f5e32bc5e371676dee553b8eaf89fea3673a7522
Masalu <pan.yiping@hotmail.com>
2021-08-03 18:23:28 +0800

    A

--- RealWorldHaskell/Ch6/read.hs
+++ RealWorldHaskell/Ch6/read.hs
@@ -2,6 +2,6 @@
 main = do
     putStrLn "Please enter a double:"
     inpStr <- getLine
-    let inpDouble = (read inpStr)::Double
+    let inpDouble = read inpStr::Double
     putStrLn ("Twice " ++ show inpDouble ++ " is " ++ show (inpDouble * 2)) 
 
--- isOdd.hs
+++ isOdd.hs
@@ -1,9 +1,10 @@
 import System.IO
 
+
 main = do 
     putStr "Please in put an Interger:"
     hFlush stdout 
     input <- getLine 
-    let number = read input::Int
+    let number = read input::Int    -- read function 可以換型別
         result = if number `mod` 2 == 0 then "Even" else "Odd"
     putStrLn (input ++ " is " ++ result ++ "!!")
